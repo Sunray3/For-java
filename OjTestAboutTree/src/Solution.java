53514a1e187626288dbad75143bd240df6bd8044
Sunray3 <sunrui_3@icloud.com>
2019-12-17 23:12:12 +0800

    Change

--- OjTestAboutTree/src/Solution.java
+++ OjTestAboutTree/src/Solution.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
- import java.util.List;
+import java.util.*;
 
 public class Solution {
 
@@ -7,6 +6,23 @@ public class Solution {
     int val;
     TreeNode left;
     TreeNode right;
+    //二叉树的前序遍历，非递归迭代实现 。
+    public List<Integer> preorderTraversalNor (TreeNode root){
+        List<Integer> list = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while(cur!= null || !stack.empty()){
+            while((cur != null )){
+                stack.push(cur);
+                System.out.println(cur.val + " ");
+                list.add(cur.val);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            cur = cur.right;
+        }
+        return list;
+    }
 //给定一个二叉树，返回它的 前序遍历。
     public List<Integer> preorderTraversal(TreeNode root) {
         List<Integer> List =  new ArrayList<>();
@@ -21,6 +37,23 @@ public class Solution {
         List.addAll(list2);
         return List;
     }
+    //二叉树中序遍历 ，非递归迭代实现。
+        public List<Integer> inorderTraversalNor (TreeNode root){
+            List<Integer> list = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+            while(cur!= null || !stack.empty()){
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.pop();
+                list.add(cur.val);
+                cur = cur.right;
+
+            }
+            return list;
+        }
     //给定一个二叉树，返回它的中序 遍历。
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list =  new ArrayList<>();
@@ -35,6 +68,32 @@ public class Solution {
         list.addAll(list2);
         return list;
     }
+// 二叉树的后序遍历 ，非递归迭代实现。
+        public List<Integer> postorderTraversalNor(TreeNode root) {
+            List<Integer> list = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            TreeNode cur = root;
+            TreeNode prev = null;
+            while(cur!= null || !stack.empty()){
+                while (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                }
+                cur = stack.peek();
+                if(cur.right == null || cur.right == prev){
+                    stack.pop();
+                    System.out.println(cur.val + " ");
+                    list.add(cur.val);
+                    prev = cur;
+                    cur = null;
+                }else{
+                    cur = cur.right;
+                }
+
+
+            }
+            return list;
+        }
     //给定一个二叉树，返回它的后序 遍历。
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> List =  new ArrayList<>();
@@ -50,7 +109,34 @@ public class Solution {
         List.add(root.val);
         return List;
     }
-    //给定两个二叉树,如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的
+    //层序遍历:给定一个二叉树，返回其按层次遍历的节点值。
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List <List<Integer>> ret =  new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        if(root != null){
+            queue.offer(root);
+        }
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            List <Integer> list =  new ArrayList<>();
+            while(size>0){
+                TreeNode cur =  queue.poll();
+                System.out.println(cur.val + " ");
+                list.add(cur.val);
+                size--;
+                if(cur.left!=null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right!=null){
+                    queue.offer(cur.right);
+                }
+            }
+            ret.add(list);
+        }
+        return ret;
+    }
+
+        //给定两个二叉树,如果两个树在结构上相同，并且节点具有相同的值，则认为它们是相同的
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p == null && q == null){
             return true;
@@ -87,5 +173,16 @@ public class Solution {
             return true;
         }
         return false;
+    }
+    //给定一个二叉树，找出其最大深度。
+    //二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+    public int maxDepth(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int LeftHeight = maxDepth(root.left);
+        int RightHeight = maxDepth(root.right);
+
+        return LeftHeight > RightHeight ? LeftHeight+1:RightHeight+1;
     }
 }
